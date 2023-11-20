356a2ebc01c04c4e9a8c02e4166188f9256d5148
0ddSh33p <fastsmith3@gmail.com>
2023-11-20 18:56:53 -0500

    ok

--- Assets/Prefabs/Character/PlayerController.cs
+++ Assets/Prefabs/Character/PlayerController.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed, mouse;
+    [SerializeField] private GameObject cam;
     [SerializeField] private Rigidbody rb;
-    float x,z;
+    float x,z, mX = 0,mY = 0;
 
     void FixedUpdate(){
         x = Input.GetAxisRaw("Horizontal");
         z = Input.GetAxisRaw("Vertical");
+        mX += Input.GetAxis("Mouse X") * mouse;
+        mY += Input.GetAxis("Mouse Y") * mouse;
 
-        rb.velocity = new Vector3(x * speed,rb.velocity.y,z * speed);
+        
+        rb.velocity += transform.forward * z * speed;
+        rb.velocity += transform.right * x * speed;
+
+
+
+        cam.transform.eulerAngles = new Vector3(-mY, gameObject.transform.eulerAngles.y, gameObject.transform.eulerAngles.z);
+        gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x,mX,gameObject.transform.eulerAngles.z);
     }
 }
