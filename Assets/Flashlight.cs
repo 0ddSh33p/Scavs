fb69d18c98fb12013ad9aaf35dc20bb86b5cb33d
0ddSh33p <fastsmith3@gmail.com>
2023-11-22 23:54:05 -0500

    lightZomm

--- Assets/Flashlight.cs
+++ Assets/Flashlight.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float minSize =1, maxSize =1, changeSpeed = 2;
+    private Light lit;
+
+    void Awake(){
+        lit = gameObject.GetComponent<Light>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if(Input.mouseScrollDelta.y < 0 && lit.innerSpotAngle < maxSize){
+            lit.intensity -= changeSpeed/2;
+            lit.range -= changeSpeed;
+            lit.innerSpotAngle += changeSpeed;
+            lit.spotAngle += changeSpeed;
+        }
+        else if(Input.mouseScrollDelta.y > 0 && lit.innerSpotAngle > minSize){
+            lit.intensity += changeSpeed/2;
+            lit.range += changeSpeed;
+            lit.innerSpotAngle -= changeSpeed;
+            lit.spotAngle -= changeSpeed;
+        }
     }
 }
