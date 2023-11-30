7a4644074a9066e16a1cd815912b29aec4ab2bee
0ddSh33p <fastsmith3@gmail.com>
2023-11-30 15:53:34 -0500

    preBadIdea

--- Assets/Prefabs/Robots/CrawlerParts/crawl.cs
+++ Assets/Prefabs/Robots/CrawlerParts/crawl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f08c684ea5e92cc93a2a82feb15333ac3ef52a9bd33a672997be1758af9ad5d
-size 2812
+oid sha256:13098a08cedd5fd0f7f427c05fe89c84024cb06279d8b2a488ae6de5e3ef3493
+size 3084
